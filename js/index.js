4c8eb7fd5fb9e89db639579391507e4bfe07f6ab
Ryan <ryanbowdre@gmail.com>
2018-01-11 13:09:45 -0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $('img').click(function(){
+    $(this).fadeOut(2000);
+          })
+    $('.showButton').click(function(){
+    $('img').show();
+          })
+ })
